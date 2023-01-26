39107be9032cfa3b3c2bb966e0115c98c8465f95
= <mouadbnl.2k01@gmail.com>
2023-01-26 15:26:45 +0100

    Imported colors from Figma to TailwindCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'DEFAULT': '#7F5AF0',
+          '500': '#7F5AF0'
+        },
+        accent: {
+          'DEFAULT': '#2CB67D',
+          '500': '#2CB67D'
+        },
+        gr: {
+          '900': '#16161A',
+          '800': '#25252B',
+          '700': '#32353B',
+          '600': '#44484F',
+          '500': '#58616D',
+          '400': '#94A1B2',
+          '300': '#B5BFCB',
+          '200': '#C8D1DC',
+          '100': '#E6EBF2',
+          '50': '#FFFFFE',
+        }
+      }
+    },
   },
   plugins: [],
 }
