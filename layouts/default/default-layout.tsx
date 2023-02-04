2578c00d01630980908d285c88e50dd31445b96b
= <mouadbnl.2k01@gmail.com>
2023-02-04 12:19:37 +0100

    Added google analytics

--- layouts/default/default-layout.tsx
+++ layouts/default/default-layout.tsx
@@ -1,11 +1,23 @@
 import { ReactElement } from "react"
 import DefaultHeader from "./default-header"
 import DefaultFooter from "./default-footer"
+import Script from "next/script"
 
 const DefaultLayout = ({ children }: { children: ReactElement }) => {
   return (
     <div className="antialiased">
       <DefaultHeader />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      ></Script>
+      <Script strategy="lazyOnload" id="google-analytics-script">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+      `}</Script>
       <div className="flex flex-col min-h-screen">
         <main className="flex-1 flex flex-col">{children}</main>
         <DefaultFooter />
